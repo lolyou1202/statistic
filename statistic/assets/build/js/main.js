95be1f10e2b3971a3f3d9d685db1e9c5e099b426
lolyou1202 <sasha555990@gmail.com>
2023-01-04 23:47:41 +0300

    second

--- statistic/assets/build/js/main.js
+++ statistic/assets/build/js/main.js
@@ -11,6 +11,9 @@
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _components_select__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./components/select */ "./assets/src/js/components/select.js");
 /* harmony import */ var _components_select__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_components_select__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _components_filling_line__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./components/filling-line */ "./assets/src/js/components/filling-line.js");
+/* harmony import */ var _components_filling_line__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_components_filling_line__WEBPACK_IMPORTED_MODULE_1__);
+
 
 
 /***/ }),
@@ -137,6 +140,54 @@ __webpack_require__.r(__webpack_exports__);
   htmlEl: document.documentElement,
   bodyEl: document.body
 });
+
+/***/ }),
+
+/***/ "./assets/src/js/components/filling-line.js":
+/*!**************************************************!*\
+  !*** ./assets/src/js/components/filling-line.js ***!
+  \**************************************************/
+/***/ (() => {
+
+let fillingline = function (value, start, today, end) {
+  let line = document.querySelector('.league-progress__line-current');
+  let keyFrames = document.createElement('style');
+  keyFrames.innerHTML = `
+        @keyframes linefilling {
+            0% {
+                max-width: 0;
+            }
+            100% {
+                max-width: ${value}%;
+            }
+        }
+
+        .league-progress__line-current {
+            position: absolute;
+            left: 0;
+            top: 0;
+            z-index: 10;
+            border-radius: 50px;
+            height: 10px;
+            background: var(--primary-font);
+            opacity: 30%;
+            width: ${value}%;
+            animation: linefilling 1s ease-in-out;
+        }
+    `;
+  line.appendChild(keyFrames);
+  let dateStart = document.querySelector('.league-progress__start');
+  let dateEnd = document.querySelector('.league-progress__end');
+  let dateToday = document.querySelector('.league-progress__today');
+  dateStart.textContent = start;
+  dateStart.style.left = 0;
+  dateEnd.textContent = end;
+  dateEnd.style.right = 0;
+  dateToday.textContent = today;
+  dateToday.style.left = `${value}%`;
+  dateToday.style.transform = 'translateX(-50%)';
+};
+fillingline(30, '19 Oct', '1 Jan', '14 Jun');
 
 /***/ }),
 
