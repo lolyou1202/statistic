db5f3f9a0269d5fc454919b36a7a2f383d767ef9
lolyou1202 <sasha555990@gmail.com>
2023-01-05 19:58:45 +0300

    asdsd

--- statistic/assets/build/js/main.js
+++ statistic/assets/build/js/main.js
@@ -222,6 +222,149 @@ let json = {
     loses: '43',
     lastFive: 'LLWLW',
     percent: '0.23'
+  },
+  3: {
+    position: '2',
+    team: {
+      imageUrl: '../img/pngegg.png',
+      name: 'GSW'
+    },
+    wins: '11',
+    loses: '43',
+    lastFive: 'LLWLW',
+    percent: '0.23'
+  },
+  4: {
+    position: '2',
+    team: {
+      imageUrl: '../img/pngegg.png',
+      name: 'GSW'
+    },
+    wins: '11',
+    loses: '43',
+    lastFive: 'LLWLW',
+    percent: '0.23'
+  },
+  5: {
+    position: '2',
+    team: {
+      imageUrl: '../img/pngegg.png',
+      name: 'GSW'
+    },
+    wins: '11',
+    loses: '43',
+    lastFive: 'LLWLW',
+    percent: '0.23'
+  },
+  6: {
+    position: '2',
+    team: {
+      imageUrl: '../img/pngegg.png',
+      name: 'GSW'
+    },
+    wins: '11',
+    loses: '43',
+    lastFive: 'LLWLW',
+    percent: '0.23'
+  },
+  7: {
+    position: '2',
+    team: {
+      imageUrl: '../img/pngegg.png',
+      name: 'GSW'
+    },
+    wins: '11',
+    loses: '43',
+    lastFive: 'LLWLW',
+    percent: '0.23'
+  },
+  8: {
+    position: '2',
+    team: {
+      imageUrl: '../img/pngegg.png',
+      name: 'GSW'
+    },
+    wins: '11',
+    loses: '43',
+    lastFive: 'LLWLW',
+    percent: '0.23'
+  },
+  9: {
+    position: '2',
+    team: {
+      imageUrl: '../img/pngegg.png',
+      name: 'GSW'
+    },
+    wins: '11',
+    loses: '43',
+    lastFive: 'LLWLW',
+    percent: '0.23'
+  },
+  10: {
+    position: '2',
+    team: {
+      imageUrl: '../img/pngegg.png',
+      name: 'GSW'
+    },
+    wins: '11',
+    loses: '43',
+    lastFive: 'LLWLW',
+    percent: '0.23'
+  },
+  11: {
+    position: '2',
+    team: {
+      imageUrl: '../img/pngegg.png',
+      name: 'GSW'
+    },
+    wins: '11',
+    loses: '43',
+    lastFive: 'LLWLW',
+    percent: '0.23'
+  },
+  12: {
+    position: '2',
+    team: {
+      imageUrl: '../img/pngegg.png',
+      name: 'GSW'
+    },
+    wins: '11',
+    loses: '43',
+    lastFive: 'LLWLW',
+    percent: '0.23'
+  },
+  13: {
+    position: '2',
+    team: {
+      imageUrl: '../img/pngegg.png',
+      name: 'GSW'
+    },
+    wins: '11',
+    loses: '43',
+    lastFive: 'LLWLW',
+    percent: '0.23'
+  },
+  14: {
+    position: '2',
+    team: {
+      imageUrl: '../img/pngegg.png',
+      name: 'GSW'
+    },
+    wins: '11',
+    loses: '43',
+    lastFive: 'LLWLW',
+    percent: '0.23'
+  },
+  15: {
+    position: '2',
+    team: {
+      imageUrl: '../img/pngegg.png',
+      name: 'GSW'
+    },
+    wins: '11',
+    loses: '43',
+    lastFive: 'LLWLW',
+    percent: '0.23'
   }
 };
 let fillingTable = function (json) {
@@ -229,50 +372,51 @@ let fillingTable = function (json) {
   body.innerHTML = '';
   for (let key in json) {
     let li = document.createElement('li');
+    let hr = document.createElement('hr');
     li.classList.add('standings__body-item');
     li.innerHTML = `
-            <div>
-                <span>${json[key].position}</span>
-            </div>
-            <div>
-                <span>
-                    <div class="standings__team-img"></div>
-                    <p class="standings__team-name">${json[key].team.name}</p>
-                </span>
-            </div>
-            <div>
-                <span>${json[key].wins}</span>
-            </div>
-            <div>
-                <span>${json[key].loses}</span>
-            </div>
-            <div>
-                <span>
-                    <div class="standings__last-five">${json[key].lastFive[0]}</div>
-                    <div class="standings__last-five">${json[key].lastFive[1]}</div>
-                    <div class="standings__last-five">${json[key].lastFive[2]}</div>
-                    <div class="standings__last-five">${json[key].lastFive[3]}</div>
-                    <div class="standings__last-five">${json[key].lastFive[4]}</div>
-                </span>
-            </div>
-            <div>
-                <span>${json[key].percent}</span>
-            </div>
-        `;
+      <div>
+          <span>${json[key].position}</span>
+      </div>
+      <div>
+          <span>
+              <div class="standings__team-img"></div>
+              <p class="standings__team-name">${json[key].team.name}</p>
+          </span>
+      </div>
+      <div>
+          <span>${json[key].wins}</span>
+      </div>
+      <div>
+          <span>${json[key].loses}</span>
+      </div>
+      <div>
+          <span>
+              <div class="standings__last-five">${json[key].lastFive[0]}</div>
+              <div class="standings__last-five">${json[key].lastFive[1]}</div>
+              <div class="standings__last-five">${json[key].lastFive[2]}</div>
+              <div class="standings__last-five">${json[key].lastFive[3]}</div>
+              <div class="standings__last-five">${json[key].lastFive[4]}</div>
+          </span>
+      </div>
+      <div>
+          <span>${json[key].percent}</span>
+      </div>
+    `;
+    li.querySelector('.standings__team-img').style.backgroundImage = `url(${json[key].team.imageUrl})`;
+    li.querySelectorAll('.standings__last-five').forEach(el => {
+      if (el.textContent == 'W') {
+        el.classList.add('win');
+      } else if (el.textContent == 'L') {
+        el.classList.add('lose');
+      }
+    });
     body.appendChild(li);
+    if (!(Object.keys(json).length == key)) {
+      body.appendChild(hr);
+    }
   }
-
-  // for (let i = 0; i <= Object.keys(json).length; i++) {
-  //     let li = document.createElement('li');
-  //     let div = document.createElement('div');
-
-  //     if (i == 0 || i == 2 ||i == 3 || i == 5) {
-  //         let span = document.createElement('span');
-
-  //     }
-  // }
 };
-
 fillingTable(json);
 
 /***/ }),
