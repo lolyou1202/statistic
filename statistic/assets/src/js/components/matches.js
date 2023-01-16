136b11d7b1789c785ac1328116d321cd7e8fbb66
lolyou1202 <97385179+lolyou1202@users.noreply.github.com>
2023-01-16 23:06:48 +0300

    54wtrh

--- statistic/assets/src/js/components/matches.js
+++ statistic/assets/src/js/components/matches.js
@@ -90,6 +90,59 @@ let json = [
         }
     },
     {
+        time: '15:00',
+        matches: {
+            1: {
+                comands: {
+                    1: {
+                        name: 'Golden State Warriors',
+                        standings: '2',
+                        place: 'Home',
+                        img: '../img/pngegg.png'
+                    },
+                    2: {
+                        name: 'Orlando Magick',
+                        standings: '10',
+                        place: 'Guests',
+                        img: '../img/pngegg.png'
+                    }
+                }
+            },
+            2: {
+                comands: {
+                    1: {
+                        name: 'Golden State Warriors',
+                        standings: '2',
+                        place: 'Home',
+                        img: '../img/pngegg.png'
+                    },
+                    2: {
+                        name: 'Orlando Magick',
+                        standings: '10',
+                        place: 'Guests',
+                        img: '../img/pngegg.png'
+                    }
+                }
+            },
+            3: {
+                comands: {
+                    1: {
+                        name: 'Golden State Warriors',
+                        standings: '2',
+                        place: 'Home',
+                        img: '../img/pngegg.png'
+                    },
+                    2: {
+                        name: 'Orlando Magick',
+                        standings: '10',
+                        place: 'Guests',
+                        img: '../img/pngegg.png'
+                    }
+                }
+            }
+        }
+    },
+    {
         time: '18:00',
         matches: {
             1: {
@@ -112,7 +165,7 @@ let json = [
     }
 ]
 
-let fillMatches = function (json) {
+let fillMatches1 = function (json) {
     let matches = document.querySelector('.matches');
     let saveHeightcard = 60;
     let saveHeightTime = 101;
@@ -121,6 +174,7 @@ let fillMatches = function (json) {
 
     let width = matches.offsetWidth;
     let height = matches.offsetHeight;
+    console.log(matches.scrollHeight);
     
     const canvas = document.getElementById('canvas');
     canvas.width = width;
@@ -228,6 +282,81 @@ let fillMatches = function (json) {
     context.lineWidth = 10;
     context.strokeStyle = '#DFE7F1';
     context.stroke();
+}
+
+let fillMatches = function (json) {
+    let matches = document.querySelector('.matches');
+    matches.innerHTML = '';
+
+    let midleLine = document.createElement('div');
+    midleLine.classList.add('midleLine');
+    matches.appendChild(midleLine);
+
+    for (let i = 0; i < json.length; i++) {
+        let midleLineBlock = document.createElement('div');
+        midleLineBlock.classList.add('midleLine__block');
+
+        let p = document.createElement('p');
+        p.textContent = json[i].time;
+
+        const canvas = document.createElement('canvas');
+        //canvas.width = '100%';
+        //canvas.height = '100%';
+        const context = canvas.getContext('2d');
+        context.beginPath();
+
+        midleLineBlock.appendChild(p);
+        
+        matches.appendChild(midleLineBlock);
+        
+        for (let key in json[i].matches) {
+            let match = document.createElement('div');
+            match.classList.add('matchInBlock');
+
+            match.innerHTML = `
+                <div class="matchInBlock-command">
+                    <div class="command__img"></div>
+                    <div class="command__info">
+                        <div class="command__info-name">
+                            <p>${json[i].matches[key].comands[1].name}</p>
+                            <span class="command__info-position">#
+                            ${json[i].matches[key].comands[1].standings}
+                            </span>
+                        </div>
+                        <div class="command__info-stadium">
+                        ${json[i].matches[key].comands[1].place}
+                        </div>
+                    </div>
+                </div>
+                <div class="matchInBlock-command">
+                    <div class="command__img"></div>
+                    <div class="command__info">
+                        <div class="command__info-name">
+                            <p>${json[i].matches[key].comands[2].name}</p>
+                            <span class="command__info-position">#
+                            ${json[i].matches[key].comands[2].standings}
+                            </span>
+                        </div>
+                        <div class="command__info-stadium">
+                        ${json[i].matches[key].comands[2].place}
+                        </div>
+                    </div>
+                </div>
+            `;
+
+            if (i % 2 == 1) {
+                midleLineBlock.style.margin = '0 0 20px auto';
+                midleLineBlock.style.padding = '0 20px 0 0';
+                midleLineBlock.style.alignItems = 'flex-end';
+                p.style.left = '0';
+            } else {
+
+            }
+
+            midleLineBlock.appendChild(match);
+        }
+        midleLineBlock.appendChild(canvas);
+    }
 }
 
 fillMatches(json);
